295fc2a8d335e05fd31a909d7356566f299b5d30
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-05-02 12:11:34 +0530

    Style: changed features

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h1>Instant Uploads</h1>
+      <h2>Loved Moments</h2>
+      <p>What I like about photographs is that they capture a moment that's gone forever, impossible to reproduce.</p>
     </div>
   )
 }
